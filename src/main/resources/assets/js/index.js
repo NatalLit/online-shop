d1298295fb8c49882b58eaf3372bf448d5bd5290
MLytvyno <Kolbasa32167>
2022-03-26 13:46:55 +0200

    Add UI fuctional

--- src/main/resources/assets/js/index.js
+++ src/main/resources/assets/js/index.js
@@ -1,15 +1,21 @@
 const name = document.getElementById("title");
 const price = document.getElementById("price");
-const button = document.getElementById("submitBtn");
+const productId = document.getElementById("productId");
+const addProductButton = document.getElementById("submitAddProductBtn");
+const editProductButton = document.getElementById("submitEditProductBtn");
 
-button.addEventListener('click', async (e) => {
+
+console.log("It works");
+console.log(addProductButton);
+if(addProductButton){
+addProductButton.addEventListener('click', async (e) => {
     e.preventDefault();
-    const body = JSON.stringify({name: name.value, price: price.value})
-    const url = "/products/add"
+    const body = JSON.stringify({name: name.value, price: price.value});
+    const url = "/products/add";
 
     try {
         const response = await fetch(url, {
-            method: 'POST', // или 'PUT'
+            method: 'POST',
             body,
             headers: {'Content-Type': 'application/json'}
         });
@@ -17,4 +23,30 @@ button.addEventListener('click', async (e) => {
     } catch (error) {
         console.error('Ошибка:', error);
     }
-})
+});
+}
+
+if(editProductButton){
+console.log("It works 2");
+editProductButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const body = JSON.stringify({id:productId.value, name: name.value, price: price.value});
+    const url = "/products/edit";
+    console.log(body);
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body,
+            headers: {'Content-Type': 'application/json'}
+        });
+        document.location.href = response.url;
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
+});
+}
+
+
+
+
+
